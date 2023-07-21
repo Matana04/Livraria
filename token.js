338c270389ec28defa21fb07a2783deff8604d5a
Mateus Matana <mateusmatana12@gmail.com>
2023-07-20 23:24:11 -0300

    artigo parte02

--- token.js
+++ token.js
@@ -1,25 +1,101 @@
+/**
+ * Chave do token no local storage
+ */
+const TOKEN_KEY = "TOKEN";
+
+/**
+ * Salva o token no local storage para ser pego depois
+ */
 function salvarToken(token) {
-    localStorage.setItem("TOKEN", JSON.stringify(token))
+  // precisamos converter o token para string
+  const tokenString = JSON.stringify(token);
+
+  // guardamos o token "stringificado" no local storage
+  localStorage.setItem(TOKEN_KEY, tokenString);
 }
 
+/**
+ * Carrega o token guardado no local storage
+ */
 function carregarToken() {
-    const token = localStorage.getItem("TOKEN")
-    return JSON.parse(token)
+  // pegamos a string do token
+  const token = localStorage.getItem("TOKEN");
+  // convertermos ela em um objeto do javascript
+  return JSON.parse(token);
 }
 
-async function enviarRequisicaoAutenticada(url) {
-    const token = carregarToken();
+/**
+ * Envia uma requisição GET para a URL, usando o token guardado no local storage
+ */
+async function getAutenticado(url) {
+  const token = carregarToken();
 
-    const resposta = await fetch(
-        url,
-        {
-          method: "GET",
-          headers: {
-            "Authorization": `Bearer ${token.access}`
-          }
-        }
-    );
+  const resposta = await fetch(url, {
+    method: "GET",
+    headers: {
+      // devemos incluir o token no cabeçalho authorization para que tenhamos acesso à API
+      Authorization: `Bearer ${token.access}`,
+    },
+  });
 
-    return resposta.json();
+  // retornamos a resposta
+  return resposta.json();
 }
 
+/**
+ * Envia uma requisição POST para a URL, usando o token guardado no local storage
+ */
+async function postAutenticado(url, body) {
+  const token = carregarToken();
+
+  const resposta = await fetch(url, {
+    method: "POST",
+    headers: {
+      // devemos incluir o token no cabeçalho authorization para que tenhamos acesso à API
+      Authorization: `Bearer ${token.access}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  // retornamos a resposta
+  return resposta.json();
+}
+
+/**
+ * Envia uma requisição DELETE para a URL, usando o token guardado no local storage
+ */
+async function deleteAutenticado(url) {
+  const token = carregarToken();
+
+  const resposta = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      // devemos incluir o token no cabeçalho authorization para que tenhamos acesso à API
+      Authorization: `Bearer ${token.access}`,
+    },
+  });
+
+  // retornamos a resposta
+  return resposta.json();
+}
+
+/**
+ * Envia uma requisição PATCH para a URL, usando o token guardado no local storage
+ */
+async function patchAutenticado(url, body) {
+  const token = carregarToken();
+
+  const resposta = await fetch(url, {
+    method: "PATCH",
+    headers: {
+      // devemos incluir o token no cabeçalho authorization para que tenhamos acesso à API
+      Authorization: `Bearer ${token.access}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  // retornamos a resposta
+  return resposta.json();
+}
